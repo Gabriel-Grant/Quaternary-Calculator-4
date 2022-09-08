29a2baaae0836103e443e8c63efbf155e2893331
Cameron Allan <camallan16@gmail.com>
2022-09-08 12:32:04 -0400

    Refactored logic operator naming conventions for unit-tests

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -5,83 +5,85 @@ public class CalculatorTest {
     CalculatorLogic calc = new CalculatorLogic();
 
     @Test
-    public void conversionToQuaternaryTest(){
+    public void conversionToQuaternaryTest() {
         String expected = "1210";
         String actual = calc.toQuad("100");
         assertEquals(expected, actual);
     }
 
     @Test
-    public void conversionToDecimalTest(){
+    public void conversionToDecimalTest() {
         String expected = "100";
         String actual = calc.toDecimal("1210");
         assertEquals(expected, actual);
     }
 
     @Test
-    public void additionTest(){
+    public void additionTest() throws Exception {
         String expected = "1";
-        String actual = calc.addition("0", "1");
+        String actual = calc.twoValueOperation("0", '+', "1");
         assertEquals(expected, actual);
     }
 
+
     @Test
-    public void additionTest2(){
+    public void additionTest2() throws Exception {
         String expected = "10";
-        String actual = calc.addition("3", "1");
+        String actual = calc.twoValueOperation("3", '+', "1");
         assertEquals(expected, actual);
     }
 
     @Test
-    public void additionLargeNumberTest(){
+    public void additionLargeNumberTest() throws Exception {
         String expected = "2303";
-        String actual = calc.addition("1111", "1132");
+        String actual = calc.twoValueOperation("1111", '+', "1132");
         assertEquals(expected, actual);
     }
+
     @Test
-    public void subtractionTest(){
+    public void subtractionTest() throws Exception {
         String expected = "10";
-        String actual = calc.subtraction("22", "12");
+        String actual = calc.twoValueOperation("22", '-', "12");
         assertEquals(expected, actual);
     }
 
     @Test
-    public void subtractionTest2(){
+    public void subtractionTest2() throws Exception {
         String expected = "2";
-        String actual = calc.subtraction("13", "11");
+        String actual = calc.twoValueOperation("13", '-', "11");
         assertEquals(expected, actual);
     }
 
     @Test
-    public void subtractionLargeNumberTest(){
+    public void subtractionLargeNumberTest() throws Exception {
         String expected = "1210";
-        String actual = calc.subtraction("2101", "231");
+        String actual = calc.twoValueOperation("2101", '-', "231");
         assertEquals(expected, actual);
     }
 
     @Test
-    public void multiplicationTest(){
+    public void multiplicationTest() throws Exception {
         String expected = "210";
-        String actual = calc.multiplication("21", "10");
+        String actual = calc.twoValueOperation("21", '*', "10");
         assertEquals(expected, actual);
     }
 
     @Test
-    public void divisionTest(){
+    public void divisionTest() throws Exception {
         String expected = "13";
-        String actual = calc.division("111", "3");
+        String actual = calc.twoValueOperation("111", '/', "3");
         assertEquals(expected, actual);
     }
 
     @Test
-    public void divisionByZeroTest(){
-        String expected = "error";
-        String actual = calc.division("100", "0");
-        assertEquals(expected, actual);
+    public void divisionByZeroTest() throws Exception {
+        ArithmeticException exception = assertThrows(ArithmeticException.class, () ->{
+                calc.twoValueOperation("10", '/', "0");
+        }, "ArithmeticException was expected");
     }
 
     @Test
-    public void quatDecSanityTest(){
+    public void quatDecSanityTest() {
         String originalInput = "10";
         String quat = calc.toQuad(originalInput);
         String result = calc.toDecimal(quat);
@@ -102,5 +104,3 @@ public class CalculatorTest {
         assertEquals("123", calc.squareRoot("23121"));
     }
 }
-
-
